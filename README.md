01f107769f0351670a836f74822df54f8cbe678c
Vincenzo Picone <118441474+Vincenzopicone@users.noreply.github.com>
2023-05-04 10:36:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CineMille
+ 
---
+++ src/test/java/it/vincenzopicone/cinemille/CineMilleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vincenzopicone.cinemille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineMilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
